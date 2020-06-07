32a0571ee5db17c4f0f334dd81c97634454fe496
f6o <fluffy6h@gmail.com>
2020-06-07 13:22:12 +0900

    diff: seprate method

--- src/main/java/io/github/f6o/rescheck/DiffTool.java
+++ src/main/java/io/github/f6o/rescheck/DiffTool.java
@@ -9,12 +9,16 @@ import com.github.fge.jsonpatch.diff.JsonDiff;
 public class DiffTool {
 	
 	public static JsonNode diffJson(String a, String b) throws JsonMappingException, JsonProcessingException {
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode tree1 = mapper.readTree(a);
-		JsonNode tree2 = mapper.readTree(b);
+		JsonNode tree1 = parseJson(a);
+		JsonNode tree2 = parseJson(b);
 		JsonNode patches = JsonDiff.asJson(tree1, tree2);
 		return patches;
 	}
 	
-}
+	public static JsonNode parseJson(String json) throws JsonMappingException, JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode n = 	mapper.readTree(json);
+		return n;
+	}
 	
+}
